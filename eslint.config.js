ad8f108962099c08fb913d2098006f34a6388776
igal <igal.nikolaev@gmail.com>
2025-03-25 10:39:20 +0200

    init

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import airbnbParser from '@typescript-eslint/parser';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintPluginPrettierRecommended,
+    ],
+    languageOptions: {
+      parser: airbnbParser,
+
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint.plugin, // Explicitly include TS plugin
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // Ensure Prettier runs as ESLint rule
+      endOfLine: 'off',
+    },
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">{count}</h1>
+      <button
+        className="bg-blue-500 text-2xl text-amber-200"
+        onClick={() => setCount(count + 1)}
+      >
+        +
+      </button>
+    </>
+  );
+}
+
+export default App;
